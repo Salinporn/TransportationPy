a91b9a150067e616faab644c4c2ef88c91aa8d93
Salinpohn <65011514@kmitl.ac.th>
2023-12-28 14:28:56 +0700

    create class Taxi

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ class Walk( Transportation ):
         return 0
 
 
+class Taxi( Transportation ):
+
+    def __init__( self, start, end, distance ):
+        Transportation.__init__( self, start, end, distance)
+
+    def find_cost( self ):
+        return 40 * self.distance
    
 # main program
 
